a7c417c60c91aa8618917637a701489f69787f91
mumble.b <mumble.b@kakaocorp.com>
2020-12-23 12:02:00 +0900

    가위바위보

---
+++ gawibawibo/client.jsx
@@ -0,0 +1,12 @@
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+// const WordRelay = require('./WordRelay');
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import {hot} from 'react-hot-loader/root';
+
+import RSP from './RSP';
+// const Hot = hot(ResponseCheck);
+
+
+ReactDOM.render(<RSP/>, document.querySelector('#root'));
